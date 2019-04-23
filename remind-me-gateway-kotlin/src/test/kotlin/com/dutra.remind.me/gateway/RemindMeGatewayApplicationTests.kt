2744c109a1435332fb3d3b418b3d49f3ee8f4484
Rafael Dutra <rafaelbdutra@gmail.com>
2019-04-23 21:25:17 +1000

    Disable eureka on gateway (kotlin) test env

--- remind-me-gateway-kotlin/src/test/kotlin/com/dutra.remind.me/gateway/RemindMeGatewayApplicationTests.kt
+++ remind-me-gateway-kotlin/src/test/kotlin/com/dutra.remind.me/gateway/RemindMeGatewayApplicationTests.kt
@@ -1,12 +1,14 @@
 package com.example.demo
 
+import com.dutra.remind.me.gateway.RemindMeGatewayApplication
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
-@SpringBootTest
+@SpringBootTest(classes = arrayOf(RemindMeGatewayApplication::class),
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RemindMeGatewayApplicationTests {
 
     @Test
